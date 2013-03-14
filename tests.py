e8e87066c6483ae1123b1867d1e4451b93949b46
Garrett Robinson <garrett.f.robinson@gmail.com>
2013-03-14 16:38:13 -0700

    Unit tests for signup, index redirect to login

--- tests.py
+++ tests.py
@@ -9,14 +9,43 @@ from app import app, init_db
 class DBTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        self.db_fd, self.db_path = tempfile.mkstemp()
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + self.db_path
         app.config['TESTING'] = True
         self.app = app.test_client()
         init_db()
         
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(app.config['DATABASE'])
+        os.unlink(self.db_path)
         
+    def test_redirect_to_login(self):
+        """
+        If the user is not logged in, accessing the home page should redirect
+        them to the login page
+        """
+        rv = self.app.get('/', follow_redirects=True)
+        assert 'Login' in rv.data
+        
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+    
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+    
+    def signup(self, username, password):
+        return self.app.post('/signup', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+    
+    def test_signup(self):
+        username, password = 'guest', 'guest'
+        rv = self.signup(username, password)
+        assert 'Welcome to your home page, %s!' % username in rv.data
+
 if __name__ == '__main__':
     unittest.main()
